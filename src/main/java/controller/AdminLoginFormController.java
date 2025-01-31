ca4f57275147355c47e1b5648f174288a2bc2139
sandeepa <sanvidurath98@gmail.com>
2025-01-31 20:51:33 +0530

    first commit

---
+++ src/main/java/controller/AdminLoginFormController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+
+public class AdminLoginFormController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Hyperlink hyperClickHere;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperResetPasswordOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+
+public class LoginFormController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Hyperlink hyperClickHere;
+
+    @FXML
+    private Hyperlink hyperRegisterHere;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperRegisterHereOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperResetPasswordOnAction(ActionEvent event) {
+
+    }
+
+}
